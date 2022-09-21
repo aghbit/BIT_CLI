7b05b11d795b5581117886b36481327b2ab4dd4f
MadMach420 <mach.krzsztf@gmail.com>
2022-09-21 23:34:11 +0200

    Update parser.ts

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -4,11 +4,9 @@ import { Task } from "./classes"
 export function parsePytestOutput(err: ExecException | null, stdout: string, stderr: string): Task {
     let currTask: Task = new Task()
     
-    if (err !== null){
+    if (err === null){
         const splitLines: string[] = stdout.split("\n")
-        const splitLastLine: string[] = splitLines[splitLines.length - 1].split(",")
-        // Możliwe że ostatnia linijka zawsze pusta (przez \n na końcu przedostatniej i później nic), wtedy wystarczy zrobić [length - 2],
-        // ale szczerze mówiąc nie wiem jak to sprawdzić. Dowiemy się po skończeniu, wtedy najwyżej się zmieni tę jedną linijkę.
+        const splitLastLine: string[] = splitLines[splitLines.length - 2].split(",")
 
         for (let i = 0; i < splitLastLine.length; i++) {
             const numAndType: string[] = splitLastLine[i].split(" ")
