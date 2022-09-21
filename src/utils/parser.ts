be9acc0a406e37ec019fb64315a889e53cf9ed8d
MadMach420 <mach.krzsztf@gmail.com>
2022-09-21 19:14:30 +0200

    Update parser.ts

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -3,6 +3,36 @@ import { Task } from "./classes"
 
 export function parsePytestOutput(err: ExecException | null, stdout: string, stderr: string): Task {
     let currTask: Task = new Task()
-    // Tu parsowanie outputu z pytesta, zrobię później i na innym branchu
+    
+    if (err !== null){
+        const splitLines: string[] = stdout.split("\n")
+        const splitLastLine: string[] = splitLines[splitLines.length - 1].split(",")
+        // Możliwe że ostatnia linijka zawsze pusta (przez \n na końcu przedostatniej i później nic), wtedy wystarczy zrobić [length - 2],
+        // ale szczerze mówiąc nie wiem jak to sprawdzić. Dowiemy się po skończeniu, wtedy najwyżej się zmieni tę jedną linijkę.
+
+        for (let i = 0; i < splitLastLine.length; i++) {
+            const numAndType: string[] = splitLastLine[i].split(" ")
+            switch (numAndType[1]) {
+                case "passed":
+                    currTask.noPassedTests += Number(numAndType[0])
+                    currTask.noAllTests += Number(numAndType[0])
+                    break;
+
+                case "failed":
+                    currTask.noFailedTests += Number(numAndType[0])
+                    currTask.noAllTests += Number(numAndType[0])
+                    break;
+
+                case "skipped":
+                    currTask.noSkippedTests += Number(numAndType[0])
+                    currTask.noAllTests += Number(numAndType[0])
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    }
+
     return currTask
 }
