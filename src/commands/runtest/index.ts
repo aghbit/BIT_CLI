5b1997fe691c16d232f7e6538e21c6e2dcd6bbfc
MadMach420 <mach.krzsztf@gmail.com>
2022-09-22 17:47:43 +0200

    Create index.ts

---
+++ src/commands/runtest/index.ts
@@ -0,0 +1,26 @@
+import { Command, Flags } from "@oclif/core"
+import { Test } from "../../utils/runTests"
+
+
+export default class Runtest extends Command {
+    static flags = {
+        set: Flags.string({ char: 's', description: 'Set to test', required: true }),
+        task: Flags.string({ char: 't', description: 'Task to test', dependsOn: ['set'] })
+    }
+
+    async run() {
+        const { flags } = await this.parse(Runtest)
+
+        if (flags.set) {
+            if (flags.task) {
+                let task: string = flags.task
+                if (task.length === 1) task = "0" + flags.task
+                Test.testSingle(flags.set, task)
+            } else {
+                Test.testSet(flags.set)
+            }
+        } else {
+            Test.testAll()
+        }
+    }
+}
